43c1b30a3ea70a30f4457e0eaf5337dfba6a18dd
Rafael Santos <39917496+Rafa90k@users.noreply.github.com>
2022-02-10 01:04:27 -0300

    Adcionando o login

---
+++ pages/api/login.ts
@@ -0,0 +1,22 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+
+export default (
+    req : NextApiRequest,
+    res : NextApiResponse
+
+) => {
+    if(req.method === 'POST') {
+
+        const {login, senha} = req.body;
+
+        if(login === 'rafa@gmail.com' && senha === 'rafa1234') {
+
+            res.status(200).json({msg : 'Bem vindo, autenticação feita com suscesso!'});
+
+        }
+
+        return res.status(400).json({erro : 'Usuario ou senha não encontrado ou não cadastrado!'});
+
+    }
+    return res.status(405).json({erro : 'Metodo informado não é valido!'});
+}
